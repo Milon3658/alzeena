e20f460e77856b5fe70c036c8cacf05fe79bf2f5
Jan Mahbub Milon <jan.milon.56@gmail.com>
2024-02-04 11:08:53 +0600

    appbar add

--- lib/Presentetion/HomePage.dart
+++ lib/Presentetion/HomePage.dart
@@ -1,4 +1,6 @@
+import 'package:alzeena/Core/AppColors.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,6 +16,17 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.notifications_outlined, color: primaryColor,),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.shopping_cart_outlined, color: primaryColor,),
+          ),
+
+        ],
       ),
       body: const Center(
         child: Column(
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:alzeena/Core/AppColors.dart';
 import 'package:alzeena/Presentetion/HomePage.dart';
 import 'package:flutter/material.dart';
 
@@ -10,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Alzeena',),
+      home: const MyHomePage(title: 'Alzeena',),
     );
   }
 }
